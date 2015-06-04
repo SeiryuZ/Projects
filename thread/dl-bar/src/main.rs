a39f650e68c6612ed8241fa78b66b7c5a6c617af
Steven <SeiryuZ@hotmail.com>
2015-06-04 19:12:18 +0700

    Update dl-bar

--- thread/dl-bar/src/main.rs
+++ thread/dl-bar/src/main.rs
@@ -2,6 +2,7 @@ extern crate hyper;
 
 use std::io::prelude::*;
 use std::io::BufReader;
+use std::sync::{Arc, Mutex};
 use std::thread;
 
 use hyper::Client;
@@ -13,30 +14,60 @@ fn main() {
     let mut client = Client::new();
 
     //let mut response = client.get("http://static.rust-lang.org/dist/rust-1.0.0-x86_64-pc-windows-gnu.msi")
-    let response = client.get("http://static2.mangainn.me/mangas/324/8837/01_10_10_2011_23_31_31.jpg")
+    //let response = client.get("http://static2.mangainn.me/mangas/324/8837/01_10_10_2011_23_31_31.jpg")
+    let response = client.get("http://speedtest.singapore.linode.com/100MB-singapore.bin")
         .send().unwrap();
 
-    let mut content_length: usize  = 0;
+    let mut content_length: usize;
     {
         let ContentLength(length) = *response.headers.get::<ContentLength>().unwrap();
         content_length = length as usize;
     }
     println!("Content length is {}", content_length);
 
-    let mut buffer = [0u8; 1024];
-    let mut read_size = 0;
+    let read_size = Arc::new(Mutex::new(0));
 
-    let mut reader = BufReader::new(response);
-    loop {
-        let read_result = reader.read(&mut buffer);
-        match read_result {
-            Ok(0) => break,
-            Err(_) => break,
-            Ok(size) => {
-                read_size += size;
-                let percent: f32 = (read_size as f32 / content_length as f32) * 100.0;
-                println!("Completed {:.2}% ({}/{})", percent, read_size, content_length);
+    let shared_read_size = read_size.clone();
+
+
+    let download_handle = thread::spawn(move || {
+
+        let mut reader = BufReader::new(response);
+        loop {
+
+            let mut buffer = [0u8; 1024];
+            let read_result = reader.read(&mut buffer);
+            match read_result {
+                Ok(0) => break,
+                Err(_) => break,
+                Ok(size) => {
+                    let mut read_size = shared_read_size.lock().unwrap();
+                    *read_size += size;
+                }
             }
         }
-    }
+
+    });
+
+    let shared_read_size = read_size.clone();
+    let gui_handle = thread::spawn(move || {
+        loop {
+            let mut size;
+            {
+                let read_size = shared_read_size.lock().unwrap();
+                size = *read_size as f32
+            }
+            let percent: f32 = (size / content_length as f32) * 100.0;
+            print!("\rCompleted {:.2}% ({}/{}) ", percent, size, content_length);
+
+            if percent == 100f32 {
+                break;
+            }
+            thread::sleep_ms(100);
+        }
+    });
+
+    download_handle.join().unwrap();
+    gui_handle.join().unwrap();
+
 }
